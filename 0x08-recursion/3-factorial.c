0b8a10b12d94315e5914c9dc58854652fee96de2
Lynda-Dev <lyndaobiano99@gmail.com>
2022-11-03 18:49:03 -0700

    good

---
+++ 0x08-recursion/3-factorial.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+* factorial - computer the factorial of a number
+*
+* @num: the number to compute
+*
+* Return: the factorial if @num is greater or equals to 0, else returns -1
+*/
+int factorial(int num)
+{
+	if (num < 0)
+		return (-1);
+	else if (num == 0 || num == 1)
+		return (1);
+	else
+		return (num * factorial(num - 1));
+}
